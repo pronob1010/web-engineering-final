d8098e61b7ea559157307ffa46a6aa16fc318742
Pronob <pronobmozumder.info@gmail.com>
2021-04-05 23:35:49 +0600

    new v3

--- labfinal/mainapp/models.py
+++ labfinal/mainapp/models.py
@@ -2,12 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-TYPE=(
-        ('Type A','Type A'),
-        ('Type B','Type B'),
-        ('Type C','Type C'),
-        ('Type D','Type D'),
-        )
 class categories(models.Model):
     tittle = models.CharField(max_length=100)
 
@@ -24,8 +18,8 @@ class UserProfile(models.Model):
         ('Female','Female'),
         ('Transgender','Transgender')
         )
+        
     user = models.OneToOneField(User, on_delete=models.CASCADE,null=True)
     gender = models.CharField(max_length=100, choices=TYPE,default=None)
     contact = models.CharField(max_length=15)
     image = models.ImageField(upload_to='media/images')
-    
